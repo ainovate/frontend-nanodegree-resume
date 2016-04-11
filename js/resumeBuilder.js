8bd229bbeaa8d88a80afd1d8b34a52c5d43a7ef9
ainovate <allisviolent@gmail.com>
2016-04-12 00:12:35 +0800

    Biography quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,7 +7,25 @@
 // $("#header").prepend(formattedRole);
 // $("#header").prepend(formattedName);
 
-var data = 
-["Euler", "James", "Course Developer", "myFunc"];
+// var data = 
+// ["Euler", "James", "Course Developer", "myFunc"];
 
-$("#main").append(data.length);
+// $("#main").append(data.length);
+
+var bio = {
+	"name": "Tian Xia",
+	"age": 31,
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "010-111-987",
+		"email": "tian@example.com",
+		"github": "ainovate",
+		"twitter": "@ainovate",
+		"location": "China",
+	},
+	"welcomeMessage": "Very Nice to see you.",
+	"skills": ["joking", "procrastinate all the time", "denail everything"],
+	"bio pic": "images/fry.jpg",
+};
+
+$("#main").append(bio.name);

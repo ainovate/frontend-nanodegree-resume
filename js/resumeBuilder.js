aa89d59d367ba2bfb1d2692facea3ff7abd1f7bf
ainovate <allisviolent@gmail.com>
2016-04-13 20:39:45 +0800

    active the title

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,11 @@
-// var name = "Tian Xia";
-// var formattedName = HTMLheaderName.replace("%data%", name);
+var name = "Tian Xia";
+var formattedName = HTMLheaderName.replace("%data%", name);
 
-// var role = "Web Developer";
-// var formattedRole = HTMLheaderRole.replace("%data%", role);
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
 
 var bio = {
